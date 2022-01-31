257e49c171e9782a9347467b9f2124efc0761120
Sophie Chu <sophiechu8@gmail.com>
2022-01-31 10:53:09 -0500

    homework 1

---
+++ Week1 copy/Assets/Scripts/WASD.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASD : MonoBehaviour
+{
+    //adding public to the line below makes this number visible in unity
+    public float speed = 0.5f;
+
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+
+    // Start is called before the first frame update
+    // runs once at the start
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    // runs forever and ever
+    void Update()
+    {
+        //declares newPosition as transform.position
+        Vector3 newPosition = transform.position;
+        //Debug.Log(newPosition);
+        if (Input.GetKey(upKey))
+        {
+            newPosition.y += speed;
+
+        }
+        if (Input.GetKey(leftKey))
+        {
+            newPosition.x -= speed;
+
+        }
+        if (Input.GetKey(downKey))
+        {
+            newPosition.y -= speed;
+
+        }
+        if (Input.GetKey(rightKey))
+        {
+            newPosition.x += speed;
+
+        }
+
+        // takes the stuff I did with newPosition and puts it back into transform.position
+        transform.position = newPosition;
+    }
+
+}
